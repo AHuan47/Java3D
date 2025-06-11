cf17c07af944033ec4dfdd7f81d11e275f395f0f
Jasmine200407 <162100046+Jasmine200407@users.noreply.github.com>
2025-06-11 19:49:40 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,10 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Select.fxml"));
-        scene.getStylesheets().add(getClass().getResource("/css/Select.css").toExternalForm());
         Parent root = loader.load();
         
         Scene scene = new Scene(root, 1200, 800);
+        scene.getStylesheets().add(getClass().getResource("/css/Select.css").toExternalForm());
         stage.setScene(scene);
         stage.setTitle("Rubik's Cube");
         
